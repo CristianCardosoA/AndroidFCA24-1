8061018468c6f1071304f8a36b9db95f1a74a349
Your Name <you@example.com>
2024-05-20 21:12:34 -0600

    Clase de persistencia

--- Aviones/app/src/main/java/mx/fca/aviones/MainActivity.java
+++ Aviones/app/src/main/java/mx/fca/aviones/MainActivity.java
@@ -1,24 +1,23 @@
 package mx.fca.aviones;
 
+import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnNext;
     Plano plano;
-
     RecyclerView listaAviones;
-
     AvionAdapter adapter;
 
     @Override
+    @Table("usuarios")
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -33,5 +32,7 @@ public class MainActivity extends AppCompatActivity {
             plano = plano.next();
             adapter.notifyDataSetChanged();
         });
+
+
     }
 }
---
+++ Persistencia/app/src/main/java/mx/fca/persistencia/Llaves.java
@@ -0,0 +1,7 @@
+package mx.fca.persistencia;
+
+public class Llaves {
+
+    public static String miLlave = "mx.fca.miLlave";
+    public static String incrementadorShared = "mx.fca.incrementador";
+}
